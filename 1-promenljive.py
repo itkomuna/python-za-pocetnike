0f7ef9e87aabed7114967debf0b17410c5861a62
Korisnik <itkomunanvoisad@gmail.com>
2024-01-24 11:00:41 +0100

    promenljive

---
+++ 1-promenljive.py
@@ -0,0 +1,19 @@
+# promeljive ili varijable
+# ime = 'Sloba'
+# starost = 46
+# visina = 1.78
+# ime = 'Mirko'
+# starost  = 58
+# visina = 200
+# print(ime + ' je visok ' + str(visina) + 'cm' + ' i ima ' + str(starost) + ' godina')
+# ime = input('Unesi ime')
+# godine = input('Unesi godine')
+# visina = input('Unesi visinu')
+# print(ime)
+# print(godine)
+# print(visina)
+
+num1 = int(input('Unesi prvi broj:'))
+num2 = int(input('Unesi drugi broj'))
+rezultat = num1 + num2
+print('Rezultat sabiranja je', rezultat)
