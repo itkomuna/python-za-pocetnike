8e8c8345272d62e7d66c8b34b0ccf6839a3b6862
Korisnik <itkomunanvoisad@gmail.com>
2024-01-24 11:02:21 +0100

    odkomentarsiao sam kod

--- 1-promenljive.py
+++ 1-promenljive.py
@@ -1,17 +1,17 @@
 # promeljive ili varijable
-# ime = 'Sloba'
-# starost = 46
-# visina = 1.78
-# ime = 'Mirko'
-# starost  = 58
-# visina = 200
-# print(ime + ' je visok ' + str(visina) + 'cm' + ' i ima ' + str(starost) + ' godina')
-# ime = input('Unesi ime')
-# godine = input('Unesi godine')
-# visina = input('Unesi visinu')
-# print(ime)
-# print(godine)
-# print(visina)
+ime = 'Sloba'
+starost = 46
+visina = 1.78
+ime = 'Mirko'
+starost  = 58
+visina = 200
+print(ime + ' je visok ' + str(visina) + 'cm' + ' i ima ' + str(starost) + ' godina')
+ime = input('Unesi ime')
+godine = input('Unesi godine')
+visina = input('Unesi visinu')
+print(ime)
+print(godine)
+print(visina)
 
 num1 = int(input('Unesi prvi broj:'))
 num2 = int(input('Unesi drugi broj'))
